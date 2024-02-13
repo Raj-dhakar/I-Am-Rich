de9fa8a08dbf065abfed71307c512015051d1e25
Raj-dhakar <rajdhakar062003@gmail.com>
2024-02-13 22:20:49 +0530

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by RAJ DHAKAR on 13/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
